de04e428afdd5f8428543371994a538aec2816e7
Elzat-developer <fergenson.2020@gmail.com>
2024-05-27 08:49:04 +0500

    first commit

---
+++ src/test/java/com/example/sweter/SweterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sweter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
