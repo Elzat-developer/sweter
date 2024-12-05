21f3aa00ecd165990562c321d4e1638a8c5f3e51
Elzat-developer <fergenson.2020@gmail.com>
2024-12-05 11:36:51 +0500

    Все локально проект развернул использовал для этого Docker.

--- src/main/resources/db/migration/V2__create_admin.sql
+++ src/main/resources/db/migration/V2__create_admin.sql
@@ -1,4 +1,4 @@
-insert into users (id , username, password, active,email)
+insert ignore into users (id , username, password, active,email)
         values (1,'admin','123',true,'elzat.sayatov.3@gmail.com');
-insert into user_role (user_id, roles)
+insert ignore into user_role (user_id, roles)
         values (1,'USER'), (1, 'ADMIN');
